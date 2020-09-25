f4a80b1c4276497b0c3dc2f39dc9e659ffb2cfe3
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-09-25 10:31:44 -0500

    add checkout page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import './App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
@@ -43,6 +44,7 @@ class App extends React.Component {
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
+					<Route exact path="/checkout" component={CheckoutPage} />
 					<Route
 						exact
 						path="/signin"
