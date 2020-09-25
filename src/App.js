830e02548ec17bc3630d0b0aea172bdd592083d4
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-09-25 07:58:50 -0500

    add user selectors

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import { auth } from './firebase/firebase.utils';
 import { createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
+import { createStructuredSelector } from 'reselect';
 
 class App extends React.Component {
 	unsubscribeFromAuth = null;
@@ -52,8 +54,8 @@ class App extends React.Component {
 	}
 }
 
-const mapStateToProps = ({ user }) => ({
-	currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+	currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = (dispatch) => ({
