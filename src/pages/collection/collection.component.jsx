5db71dcbc5e494648f702277c4478a22e8e62a11
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-09-26 15:48:25 -0500

    add nested routing in shop page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './collection.styles.scss';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+const CollectionPage = ({ match }) => (
+	<div className="collection-page">
+		<h2>Collection PAGE</h2>
+	</div>
+);
+
+export default CollectionPage;
