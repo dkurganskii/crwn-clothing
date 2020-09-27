8ba6cd144750b3d8fbec9cbc7c800b360459debc
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-09-27 14:08:17 -0500

    bug fix for CollectionPreview

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import MenuItem from '../../components/menu-item/menu-item.component';
-import './directory.styles.scss';
 import { connect } from 'react-redux';
-import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
 
 const Directory = ({ sections }) => (
 	<div className="directory-menu">
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import './collection.styles.scss';
-import CollectionItem from '../../components/collection-item/collection-item.component';
 import { connect } from 'react-redux';
-import { selectCollection } from '../../redux/shop/shop.selectores';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
 	const { title, items } = collection;
@@ -13,6 +16,7 @@ const CollectionPage = ({ collection }) => {
 		</div>
 	);
 };
+
 const mapStateToProps = (state, ownProps) => ({
 	collection: selectCollection(ownProps.match.params.collectionId)(state)
 });
