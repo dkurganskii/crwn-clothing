2bd1a5fb1f0a8216556c8460ba81062c18eaea35
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-09-28 11:49:46 -0500

    fix sign in with google button

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+
 import './custom-button.styles.scss';
 
 const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
-	<button className={`${inverted ? 'inverted' : ''} custom-button`} {...otherProps}>
+	<button
+		className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+		{...otherProps}
+	>
 		{children}
 	</button>
 );
