64d79d6e3c88912e9c289b807837e6be75e59a44
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-10-04 09:14:04 -0500

    add selectIsCollectionsLoaded

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -13,3 +13,5 @@ export const selectCollection = (collectionUrlParam) =>
 	createSelector([ selectCollections ], (collections) => (collections ? collections[collectionUrlParam] : null));
 
 export const selectIsCollectionFetching = createSelector([ selectShop ], (shop) => shop.isFetching);
+
+export const selectIsCollectionsLoaded = createSelector([ selectShop ], (shop) => !!shop.collections);
