b320a186fcdd20f5e8768e70b709897ef1a9bbbf
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-01-28 18:57:50 -0600

    test

--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { toast } from "react-toastify";
 
 import { addItem } from '../../redux/cart/cart.actions';
-
 import {
 	CollectionItemContainer,
 	CollectionFooterContainer,
